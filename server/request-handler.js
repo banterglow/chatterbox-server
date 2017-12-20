5648d68d245904db14f14ff9a7671b188ac9704a
wongal5 <a.wong9970@gmail.com>
2017-12-19 19:39:31 -0800

    Cleaned up and added comments

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,17 +1,3 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-
 var url = require('url');
 var fs = require('fs');
 
@@ -41,6 +27,8 @@ var requestHandler = function(request, response) {
   //  ====================================================================
   //  Handle Invalid Endpoint Requests
   //  ====================================================================
+
+  // Essentially a makeshift router.
   if (address === '/') {
     fs.readFile('../client/index.html', function(err, data) {
       response.writeHead(200, {'Content-Type': 'text/html'});
@@ -83,7 +71,6 @@ var requestHandler = function(request, response) {
     //  Handle GET Requests
     //  ====================================================================
     } else if (request.method === 'GET') {
-
       var statusCode = 200;
       headers['Content-Type'] = 'application/json';
       response.writeHead(statusCode, headers);
@@ -104,7 +91,9 @@ var requestHandler = function(request, response) {
       // when stream is done buffering, store the message and 
       request.on('end', () => {
         try {
+          // parse the body into a message object
           let receivedData = JSON.parse(body);
+          // check if required fields are present on the message
           if (!receivedData.username || !receivedData.message || !receivedData.roomname || !receivedData.createdAt) { // if doesn't have username, text, roomname, createdAt
             throw 'Required data property is missing';
           } 
@@ -123,32 +112,5 @@ var requestHandler = function(request, response) {
   }
 };
 
-
-
+// exports request handler function so that it can be imported using 'require' in other files.
 exports.requestHandler = requestHandler;
-
-
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-
-    // .writeHead() writes to the request line and headers of the response,
-    // which includes the status and all headers.
-    // response.writeHead(statusCode, headers);
-
-    // Make sure to always call response.end() - Node may not send
-    // anything back to the client until you do. The string you pass to
-    // response.end() will be the body of the response - i.e. what shows
-    // up in the browser.
-    //
-    // response.write(JSON.stringify(messageObj));
-    // Calling .end "flushes" the response's internal buffer, forcing
-    // node to actually send all the data over to the client.
-    // response.end();
-
